fa5e3ec16673921cebc0ea8ab258c30c8392fe58
ShivaniPatid <shivanipatidar712@gmail.com>
2023-01-24 11:00:28 +0530

    Exception handling

---
+++ ExceptionHnadlingexample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ExceptionHnadlingexample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestException.TestArgumentException("shi");
+            int[] arr = new int[3];
+
+            try
+            {
+                Console.WriteLine("Enter First Number");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Second Number");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                arr[0] = 1;
+                arr[1] = 2;
+                arr[2] = 3;
+
+                int result = num1 / num2;
+                Console.WriteLine("Result : "+result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+        }
+    }
+
+}
---
+++ ExceptionHnadlingexample/TestException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHnadlingexample
+{
+    internal class TestException
+    {
+        public static void TestArgumentException(string name)
+        {
+            try
+            {
+                string result = name.Substring(name.Length + 2);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
