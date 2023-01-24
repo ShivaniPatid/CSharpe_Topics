3c11005e8ad9540efcbadeb69937852be878ae7c
ShivaniPatid <shivanipatidar712@gmail.com>
2023-01-24 11:00:54 +0530

    Reflection

---
+++ ReflectionDemo/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReflectionDemo
+{
+    internal class Customer
+    {
+        //properties
+        public int ID { get; set; }
+        public string Name { get; set; }
+        //constructors
+        public Customer()
+        {
+            this.ID = -1;
+            this.Name = string.Empty;
+        }
+
+        public Customer(int id, string name)
+        {
+            this.ID = id;
+            this.Name = name;
+
+        }
+
+
+        //methods
+        public void PrintID()
+        {
+            Console.WriteLine("ID is {0}", this.ID);
+        }
+        public void PrintName()
+        {
+            Console.WriteLine("Name is {0}", this.Name);
+        }
+    }
+}
---
+++ ReflectionDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //variable metadeta
+            int a = 20;
+            Type type = a.GetType();
+            Console.WriteLine(type);
+            Console.WriteLine(type.Assembly);
+            Console.WriteLine(type.Name);
+            Console.WriteLine(type.IsValueType);
+            Console.WriteLine(type.IsEnum);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Type types = typeof(Customer);
+            ConstructorInfo[] constructorInfos = types.GetConstructors(BindingFlags.Public | BindingFlags.Instance);
+            foreach (ConstructorInfo constructor in constructorInfos)
+            {
+                Console.WriteLine(constructor);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            MethodInfo[] methodInfos = types.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+            foreach (MethodInfo method in methodInfos)
+            {
+                Console.WriteLine(method);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            PropertyInfo[] propertyInfo = types.GetProperties();
+            foreach (PropertyInfo property in propertyInfo)
+            {
+                Console.WriteLine(property);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
