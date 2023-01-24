05c2f3c7ceffe5316fedf9f4cc46e4b924249b0e
ShivaniPatid <shivanipatidar712@gmail.com>
2023-01-24 10:59:54 +0530

    Data Annotations example

---
+++ DataannotationsExample/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataannotationsExample
+{
+    internal class Employee
+    {
+        [Required(ErrorMessage = "Employe {0} is required")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Number of characters sholud be minimus 3 characters")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Range(18, 99, ErrorMessage = "Age should be between 18 to 99")]
+        public int Age { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
---
+++ DataannotationsExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace DataannotationsExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Name = "Shivani";
+            employee.Age = 20;
+            employee.PhoneNumber = "62641abcd";
+            employee.Email = "shivani@gmail";
+
+            ValidationContext context = new ValidationContext(employee, null, null);
+            List<ValidationResult> result = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(employee, context, result, true);
+
+            if (!valid)
+            {
+                foreach (ValidationResult vr in result)
+                {
+                    Console.WriteLine("Member name {0}", vr.MemberNames.First());
+                    Console.WriteLine("Error message {0} {1}", vr.ErrorMessage, Environment.NewLine);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Name :" + employee.Name + ", Age : " + employee.Age + ", Phone Number : " + employee.Name + ", Email : " + employee.Email);
+            }
+        }
+            
+
+    }
+}
