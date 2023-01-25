5ed35dbf8c6ab52a989c6efeeb985b779ed7d7b0
ShivaniPatid <shivanipatidar712@gmail.com>
2023-01-25 08:39:41 +0530

    LINQ

---
+++ LINQExample/AddressBook.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace LINQExample
+{
+    internal class AddressBook
+    {
+        public readonly DataTable dataTable = new DataTable();
+
+        //edit existing contact person
+        public void UpdateContact(List<Contacts> contactList)
+        {
+            var data = contactList.Where(s => s.FirstName == "Mohan").Select(s => { { s.Address = "Mumbai"; } { s.City = "Mumbai"; } { s.Zip = "456601"; } return s; });
+
+
+            foreach (var contact in data)
+            {
+                Console.WriteLine("First Name : " + contact.FirstName + " Last Name : " + contact.LastName + " Address : " + contact.Address
+                    + " City : " + contact.City + " State : " + contact.State + " Zip : " + contact.Zip + " Phone Number : " + contact.PhoneNumber
+                    + " Email : " + contact.email);
+            }
+        }
+
+        //delete a person using person's name
+        public void DeleteContact(List<Contacts> contactList)
+        {
+            var data = contactList.Where(x => x.FirstName != "Mohan").ToList();
+
+            foreach (var contact in data)
+            {
+                Console.WriteLine("First Name : " + contact.FirstName + " Last Name : " + contact.LastName + " Address : " + contact.Address
+                    + " City : " + contact.City + " State : " + contact.State + " Zip : " + contact.Zip + " Phone Number : " + contact.PhoneNumber
+                    + " Email : " + contact.email);
+            }
+        }
+
+        //Retrieve Person belonging to a City or State from the Address Book
+        public void RetrievePerson(List<Contacts> contactList)
+        {
+            var data = from listContact in contactList
+                       where (listContact.City == "Indore" && listContact.State == "Madhya Pradesh")
+                       select listContact;
+            foreach (var contact in data)
+            {
+                Console.WriteLine("First Name : " + contact.FirstName + " Last Name : " + contact.LastName + " Address : " + contact.Address
+                    + " City : " + contact.City + " State : " + contact.State + " Zip : " + contact.Zip + " Phone Number : " + contact.PhoneNumber
+                    + " Email : " + contact.email);
+            }
+        }
+
+        //understand the size of address book by City 
+
+        public void SizeOfAddressBookByCity(List<Contacts> contactList)
+        {
+            var data = contactList.GroupBy(x => x.City).Select(x => new { City = x.Key, Count = x.Count() });
+            foreach (var contact in data)
+            {
+                Console.WriteLine(contact.City + "------" + contact.Count);
+            }
+        }
+
+        //understand the size of address book by State
+
+        public void SizeOfAddressBookByState(List<Contacts> contactList)
+        {
+            var data = contactList.GroupBy(x => x.State).Select(x => new { State = x.Key, Count = x.Count() });
+            foreach (var contact in data)
+            {
+                Console.WriteLine(contact.State + "------" + contact.Count);
+            }
+        }
+
+        //retrieve entries sorted alphabetically by Person’s name for a given city
+
+        public void RetrieveEntries(List<Contacts> contactList)
+        {
+            var data = contactList.OrderBy(city => city.City).ThenBy(name => name.FirstName);
+            foreach (var contact in data)
+            {
+                Console.WriteLine("First Name : " + contact.FirstName + " Last Name : " + contact.LastName + " Address : " + contact.Address
+                    + " City : " + contact.City + " State : " + contact.State + " Zip : " + contact.Zip + " Phone Number : " + contact.PhoneNumber
+                    + " Email : " + contact.email);
+            }
+        }
+
+
+
+    }
+}
---
+++ LINQExample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Contacts> contacts = new List<Contacts>()
+            { 
+                //insert the contacts in address book
+                new Contacts(){FirstName="Rita",LastName="Mehta",Address="Indore",City="Indore",State="Madhya Pradesh",Zip="456010",PhoneNumber="8896564552",email="rita@gmail.com"},
+                new Contacts(){FirstName="Riya",LastName="Sharma",Address="Ujjain",City="Ujjain",State="Madhya Pradesh",Zip="456661",PhoneNumber="8569741526",email="riya@gmail.com"},
+                new Contacts(){FirstName="Ajay",LastName="Sharma",Address="Indore",City="Indore",State="Madhya Pradesh",Zip="456010",PhoneNumber="9927845621",email="ajay@gmail.com"},
+                new Contacts(){FirstName="Vijay",LastName="Vyas",Address="Ujjain",City="Ujjain",State="Madhya Pradesh",Zip="456661",PhoneNumber="6261563458",email="vijay@gmail.com"},
+                new Contacts(){FirstName="Mohan",LastName="Dube",Address="Pune",City="Pune",State="Maharastra",Zip="456601",PhoneNumber="1234567890",email="mohan@gmail.com"},
+
+            };
+
+            /*
+             foreach (Contacts contact in contacts)
+             {
+                Console.WriteLine("First Name : " + contact.FirstName + " Last Name : " + contact.LastName + " Address : " + contact.Address
+                    + " City : " + contact.City + " State : " + contact.State + " Zip : " + contact.Zip + " Phone Number : " + contact.PhoneNumber
+                    + " Email : " + contact.email);
+            
+             }*/
+
+            AddressBook addressBook = new AddressBook();
+
+            //edit existing contact person
+            addressBook.UpdateContact(contacts);
+
+
+            //delete a person using person's name
+            addressBook.DeleteContact(contacts);
+
+
+            //Retrieve Person belonging to a City or State from the Address Book
+            addressBook.RetrievePerson(contacts);
+
+
+            //understand the size of address book by City 
+            addressBook.SizeOfAddressBookByCity(contacts);
+
+
+            //understand the size of address book by State
+            addressBook.SizeOfAddressBookByState(contacts);
+
+            //retrieve entries sorted alphabetically by Person’s name for a given city
+            addressBook.RetrieveEntries(contacts);
+        }
+    }
+}
