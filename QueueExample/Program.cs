66a0245fb641ae25333730f32f3a69ed7ae002e4
ShivaniPatid <shivanipatidar712@gmail.com>
2023-01-26 10:36:44 +0530

    Queue

---
+++ QueueExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace QueueExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue("Shivani");
+            queue.Enqueue("Patidar");
+            queue.Enqueue('s');
+            queue.Enqueue(1903);
+            queue.Enqueue(17.19);
+            foreach(var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("After deleting the element");
+            queue.Dequeue();
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
