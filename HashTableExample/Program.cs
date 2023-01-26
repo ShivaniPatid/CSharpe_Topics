36ba57f456a920fc5ab4a4b7b5bdc594ba3bbb06
ShivaniPatid <shivanipatidar712@gmail.com>
2023-01-26 10:35:50 +0530

    Hashtable

---
+++ HashTableExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTableExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("A1", "Hello");
+            hashtable.Add("A2", "Good Morning");
+            hashtable.Add("A3", "India");
+            foreach(DictionaryEntry entry in hashtable)
+            {
+                Console.WriteLine("{0} and {1}",entry.Key, entry.Value);
+            }
+            Console.WriteLine("After deleting the element");
+            hashtable.Remove("A3");
+            foreach (DictionaryEntry entry in hashtable)
+            {
+                Console.WriteLine("{0} and {1}", entry.Key, entry.Value);
+            }
+        }
+    }
+}
