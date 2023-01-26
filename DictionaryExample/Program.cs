89f67eedbf06b3ad139c4c2ec1c915cb83e42293
ShivaniPatid <shivanipatidar712@gmail.com>
2023-01-26 10:35:23 +0530

    Dictionary

---
+++ DictionaryExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("a.01", "C");
+            dic.Add("a.02", "C++");
+            dic.Add("a.03", "C#");
+            foreach(KeyValuePair<string, string> pair in dic)
+            {
+                Console.WriteLine("{0} and {1}",pair.Key, pair.Value);
+            }
+        }
+    }
+}
