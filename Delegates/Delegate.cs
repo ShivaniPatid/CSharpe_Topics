d772ab72b67b1019fd5b642a158658d2ed7a9a8e
ShivaniPatid <shivanipatidar712@gmail.com>
2023-01-26 10:34:48 +0530

    Delegats

---
+++ Delegates/Delegate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates
+{
+    //Declare a delegate type
+    delegate int Operation(int x, int y);
+    internal class Delegate
+    {
+        public void delegatesE()
+        {
+            //create a instance of the delegate that refers to the Add method.
+            Operation op = Add;
+
+            int result = op(3, 4);
+            Console.WriteLine(result);
+        }
+
+        //A method thatmatches that delegate's signature
+        static int Add(int x, int y) => x + y;
+    }
+}
+
+
+
+//In C#, a delegate is a type that represents a reference to a method. 
+//Delegates are used to pass methods as arguments to other methods, and to create event-handler methods.        
---
+++ Delegates/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Delegates
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Delegate d = new Delegate();
+            d.delegatesE();
+
+        }
+    }
+
+}
