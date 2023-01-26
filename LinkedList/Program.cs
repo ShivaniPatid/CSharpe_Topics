5bc8ed6d4b75f46a311ac8f040da22251176d0b7
ShivaniPatid <shivanipatidar712@gmail.com>
2023-01-26 10:36:16 +0530

    Linked List

---
+++ LinkedList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> list = new LinkedList<string>();
+
+            list.AddLast("Shivani");
+           var newNode = list.AddLast("Arpita");
+            list.AddLast("Rajat");
+            list.AddLast("Kuldeep");
+            list.AddLast("Aman");
+            list.AddFirst("Aastha");
+            list.AddBefore(newNode, "Rajat");
+            foreach(string str in list)
+            {
+                Console.WriteLine(str);
+            }
+
+            list.RemoveFirst();
+            list.RemoveLast();
+            list.Remove("Rajat");
+            Console.WriteLine("----------------------");
+            foreach (string str in list)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
